f8f4feef0e32c02a7fc25512829bcc7aa2ed6ef7
fionanealon <37928647+fionanealon@users.noreply.github.com>
2018-04-29 18:43:43 +0100

    Add files via upload

---
+++ totalplot.py
@@ -0,0 +1,18 @@
+# Fiona Nealon, 2018-04-28
+# Iris data set analysis
+# Plot histograms and scatterplots to analyse iris data set
+
+# Use seaborn library to analyse data
+import seaborn as sns
+
+# Use matplotlib & pyplot libraries to plot histograms and scatter plot
+import matplotlib.pyplot as plt
+
+# Read csv file into python using numpy
+iris = sns.load_dataset("iris")
+
+# Plot histograms ad scatter plots
+sns.pairplot(iris, hue="species")
+
+# Output the data
+plt.show()
